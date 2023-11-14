48de88fa04d0f6503c7fb5b241b8b9b2d52722a1
“Darkidd77” <m.almahdi77@yahoo.com>
2023-11-14 10:50:24 -0800

    new print_intgere

--- print_int.c
+++ print_int.c
@@ -7,32 +7,30 @@
  */
 int print_integer(va_list args)
 {
-    int n = va_arg(args, int);
-    int num = (n < 0) ? -n : n;  // Absolute value of n
-    int last_digit, exp = 1, count = 0;
+	int n = va_arg(args, int);
+	int num = (n < 0) ? -n : n;
+	int last_digit, exp = 1, count = 0;
 
-    if (n < 0)
-    {
-        _putchar('-');
-        count++;
-    }
+	if (n < 0)
+	{
+		_putchar('-');
+		count++;
+	}
 
-    // Find the place value of the leftmost digit
-    do
-    {
-        exp *= 10;
-    } while (num / exp != 0);
+	do {
+		exp *= 10;
+	}
 
-    // Print each digit
-    do
-    {
-        exp /= 10;
-        last_digit = num / exp;
-        _putchar(last_digit + '0');
-        count++;
-        num %= exp;
-    } while (exp > 1);
+	while (num / exp != 0);
 
-    return count;
+	do {
+		exp /= 10;
+		last_digit = num / exp;
+		_putchar(last_digit + '0');
+		count++;
+		num %= exp;
+	}
+
+	while (exp > 1);
+	return (count);
 }
-
