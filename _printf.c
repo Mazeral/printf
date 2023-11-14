845c610089324717097c00db28a9cb511664342c
MohammadOSA <mohammadsiddig5026485@gmail.com>
2023-11-14 21:30:40 +0200

    Created multiple files and multiple functionalities

--- _printf.c
+++ _printf.c
@@ -1,32 +1,46 @@
 #include "main.h"
-#include<stdarg.h>
-
 /**
- * _printf - a mimic function for the real printf
- * @s: The string
- * Return: The number of characters printed
+ * _printf - is a function that selects the correct function to print.
+ * @format: identifier to look for.
+ * Return: the length of the string.
  */
-int _printf(const char *s, ...)
+int _printf(const char * const format, ...)
 {
-va_list args;
-unsigned int i;
-int p = 0;
-va_start(args, s);
-if (s == NULL)
-return (-1);
-for (i = 0; s[i] != '\0'; i++)
-{
-if (s[i] == '%')
-{
-p += format(s[i + 1], args);
-i++;
-}
-else
-{
-_putchar(s[i]);
-p++;
-}
-}
-va_end(args);
-return (p);
+	convert p[] = {
+		{"%s", print_s}, {"%c", print_c},
+		{"%%", print_37},
+		{"%i", print_i}, {"%d", print_d}, {"%r", print_revs},
+		{"%R", print_rot13}, {"%b", print_bin},
+		{"%u", print_unsigned},
+		{"%o", print_oct}, {"%x", print_hex}, {"%X", print_HEX},
+		{"%S", print_exc_string}, {"%p", print_pointer}
+	};
+
+	va_list args;
+	int i = 0, j, length = 0;
+
+	va_start(args, format);
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+		return (-1);
+
+Here:
+	while (format[i] != '\0')
+	{
+		j = 13;
+		while (j >= 0)
+		{
+			if (p[j].ph[0] == format[i] && p[j].ph[1] == format[i + 1])
+			{
+				length += p[j].function(args);
+				i = i + 2;
+				goto Here;
+			}
+			j--;
+		}
+		_putchar(format[i]);
+		length++;
+		i++;
+	}
+	va_end(args);
+	return (length);
 }
